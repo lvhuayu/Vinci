71d17347c768753383fac68f447de655b9154945
Huayu Lu <luhuayu@amazon.com>
2018-10-05 15:37:52 -0700

    Implement the registraion page

--- src/screens/ScreenReg/index.js
+++ src/screens/ScreenReg/index.js
@@ -1,21 +1,10 @@
 /**
- * ScreenHome/index.js
+ * ScreenReg/index.js
  */
 import React, {Component} from 'react';
-import view from "../ScreenHome/view";
+import view from "../ScreenReg/view";
 
-export default class ScreenHome extends Component {
-
-    static navigationOptions = {
-        title: '首页',
-        tabBarIcon: ({ focused }) => {
-            // 根据是否选中，显示不同图片
-            const icon = focused
-                ? require('../../assets/images/tab_home_active.png')
-                : require('../../assets/images/tab_home.png');
-            return <Image source={icon} style={{ height: 22, width: 22 }} />;
-        },
-    };
+export default class ScreenReg extends Component {
 
     constructor(props) {
         super();
@@ -25,4 +14,4 @@ export default class ScreenHome extends Component {
     render() {
         return view(this);
     }
-}
+};
